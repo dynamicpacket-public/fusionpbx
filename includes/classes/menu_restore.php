f73c307301c5a8a2becef63235ca72e12cf0c74d
markjcrane <markjcrane@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2011-04-25 16:18:31 +0000

    Update PHP class menu restore.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk/fusionpbx@1045 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- includes/classes/menu_restore.php
+++ includes/classes/menu_restore.php
@@ -51,114 +51,87 @@ include "root.php";
 				//set the variable
 					$v_id = $this->v_id;
 
-				//load the default database into a sqlite memory database
-					$filename = $_SERVER["DOCUMENT_ROOT"].PROJECT_PATH.'/includes/install/sql/sqlite.sql';
-					$file_contents = file_get_contents($filename);
-					unset($filename);
-					try {
-						$db_default = new PDO('sqlite::memory:'); //sqlite 3
-						//$db_default->beginTransaction();
-					}
-					catch (PDOException $error) {
-						print "error: " . $error->getMessage() . "<br/>";
-						die();
-					}
-
-					//replace \r\n with \n then explode on \n
-						$file_contents = str_replace("\r\n", "\n", $file_contents);
-
-					//loop line by line through all the lines of sql code
-						$stringarray = explode("\n", $file_contents);
-						$x = 0;
-						foreach($stringarray as $sql) {
-							try {
-								$db_default->query($sql);
-							}
-							catch (PDOException $error) {
-								echo "error: " . $error->getMessage() . " sql: $sql<br/>";
-								//die();
-							}
-							$x++;
-						}
-						unset ($file_contents, $sql);
-						//$db_default->commit();
-
-				//load the default menu into an array
-					$sql = "";
-					$sql .= "select * from v_menu ";
-					$prepstatement = $db_default->prepare(check_sql($sql));
-					$prepstatement->execute();
-					$menu_array = $prepstatement->fetchAll();
-
-				//use the menu array to restore the default menu
-					foreach ($menu_array as &$row) {
-						//set the variables
-							$menulanguage = $row["menulanguage"];
-							$menutitle = $row["menutitle"];
-							$menustr = $row["menustr"];
-							$menucategory = $row["menucategory"];
-							$menudesc = $row["menudesc"];
-							$menuorder = $row["menuorder"];
-							$menugroup = $row["menugroup"];
-							$menuadduser = $row["menuadduser"];
-							$menuadddate = $row["menuadddate"];
-							$menumoduser = $row["menumoduser"];
-							$menumoddate = $row["menumoddate"];
-							$menu_protected = $row["menu_protected"];
-							$menu_guid = $row["menu_guid"];
-							$menu_parent_guid = $row["menu_parent_guid"];
-
-						//if the guid is not currently in the db then add it
-							$sql = "select * from v_menu ";
-							$sql .= "where v_id = '$v_id' ";
-							$sql .= "and menu_guid = '$menu_guid' ";
-							$prepstatement = $db->prepare(check_sql($sql));
-							if ($prepstatement) {
-								$prepstatement->execute();
-								$result = $prepstatement->fetchAll(PDO::FETCH_ASSOC);
-								if (count($result) == 0) {
-									//insert the default menu into the database
-										$sql = "insert into v_menu ";
-										$sql .= "(";
-										$sql .= "v_id, ";
-										$sql .= "menulanguage, ";
-										$sql .= "menutitle, ";
-										$sql .= "menustr, ";
-										$sql .= "menucategory, ";
-										$sql .= "menugroup, ";
-										$sql .= "menudesc, ";
-										$sql .= "menuorder, ";
-										$sql .= "menuadduser, ";
-										$sql .= "menuadddate, ";
-										$sql .= "menumoduser, ";
-										$sql .= "menumoddate, ";
-										$sql .= "menu_protected, ";
-										$sql .= "menu_guid, ";
-										$sql .= "menu_parent_guid ";
-										$sql .= ")";
-										$sql .= "values ";
-										$sql .= "(";
-										$sql .= "'$v_id', ";
-										$sql .= "'$menulanguage', ";
-										$sql .= "'$menutitle', ";
-										$sql .= "'$menustr', ";
-										$sql .= "'$menucategory', ";
-										$sql .= "'$menugroup', ";
-										$sql .= "'$menudesc', ";
-										$sql .= "'$menuorder', ";
-										$sql .= "'$menuadduser', ";
-										$sql .= "'$menuadddate', ";
-										$sql .= "'$menumoduser', ";
-										$sql .= "'$menumoddate', ";
-										$sql .= "'$menu_protected', ";
-										$sql .= "'$menu_guid', ";
-										$sql .= "'$menu_parent_guid' ";
-										$sql .= ")";
-										$db->exec(check_sql($sql));
-										unset($sql);
+				//use the app array to restore the default menu
+					foreach ($_SESSION['v_apps'] as $row) {
+						foreach ($row['menu'] as $menu) {
+							//set the variables
+								$menu_title = $menu['title']['en'];
+								$menu_language = 'en';
+								$menu_guid = $menu['guid'];
+								$menu_parent_guid = $menu['parent_guid'];
+								$menu_category = $menu['category'];
+								$menu_path = $menu['path'];
+								$menu_order = $menu['order'];
+								$menu_desc = $menu['desc'];
+								$menu_group = '';
+								foreach ($menu['groups'] as &$group) {
+									if ($menu_group == "") {
+										$menu_group = $group;
+									}
 								}
-							}
-							unset($prepstatement, $result);
+								if (in_array("user", $menu['groups'])) {
+									$menu_group = "user";
+								}
+								if (in_array("user", $menu['groups']) && in_array("admin", $menu['groups'])) {
+									$menu_group = "user";
+								}
+								if (in_array("admin", $menu['admin']) && in_array("superadmin", $menu['groups'])) {
+									$menu_group = "admin";
+								}
+								if (in_array("user", $menu['groups']) && in_array("admin", $menu['groups']) && in_array("superadmin", $menu['groups'])) {
+									$menu_group = "user";
+								}
+								if (strlen($menu_order) == 0) {
+									$menu_order = 1;
+								}
+
+							//if the guid is not currently in the db then add it
+								$sql = "select * from v_menu ";
+								$sql .= "where v_id = '$v_id' ";
+								$sql .= "and menu_guid = '$menu_guid' ";
+								$prepstatement = $db->prepare(check_sql($sql));
+								if ($prepstatement) {
+									$prepstatement->execute();
+									$result = $prepstatement->fetchAll(PDO::FETCH_ASSOC);
+									if (count($result) == 0) {
+										//insert the default menu into the database
+											$sql = "insert into v_menu ";
+											$sql .= "(";
+											$sql .= "v_id, ";
+											$sql .= "menulanguage, ";
+											$sql .= "menutitle, ";
+											$sql .= "menustr, ";
+											$sql .= "menucategory, ";
+											$sql .= "menugroup, ";
+											$sql .= "menudesc, ";
+											$sql .= "menuorder, ";
+											$sql .= "menu_guid, ";
+											$sql .= "menu_parent_guid ";
+											$sql .= ") ";
+											$sql .= "values ";
+											$sql .= "(";
+											$sql .= "'$v_id', ";
+											$sql .= "'$menu_language', ";
+											$sql .= "'$menu_title', ";
+											$sql .= "'$menu_path', ";
+											$sql .= "'$menu_category', ";
+											$sql .= "'$menu_group', ";
+											$sql .= "'$menu_desc', ";
+											$sql .= "'$menu_order', ";
+											$sql .= "'$menu_guid', ";
+											$sql .= "'$menu_parent_guid' ";
+											$sql .= ")";
+
+											if ($menu_guid == $menu_parent_guid) {
+												//echo $sql."<br />\n";
+											}
+											else {
+												$db->exec(check_sql($sql));
+											}
+											unset($sql);
+									}
+								}
+						}
 					}
 			} //end function
 	} //class
