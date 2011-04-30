1cef416112912db996672264fa3f31e31e8445ab
markjcrane <markjcrane@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2011-04-30 23:23:56 +0000

    Diemos found this gold nugget called Adminer licensed under Apache 2.0 license. This tool has a great feature set for multiple databases including sqlite, mysql, postgres and other databases. Thanks diemos.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk/fusionpbx@1063 e2954ef0-caed-11de-bd98-e341d93cc3cc

---
+++ mod/adminer/v_config.php
@@ -0,0 +1,20 @@
+<?php
+	//application details
+		$apps[$x]['name'] = "Adminer";
+		$apps[$x]['guid'] = '214B9F02-547B-D49D-F4E9-02987D9581C5';
+		$apps[$x]['category'] = 'System';
+		$apps[$x]['subcategory'] = '';
+		$apps[$x]['version'] = '3.2.2';
+		$apps[$x]['license'] = 'http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0';
+		$apps[$x]['url'] = 'http://www.adminer.org/';
+		$apps[$x]['description']['en'] = 'Adminer (formerly phpMinAdmin) is a full-featured database management tool written in PHP. Adminer is available for MySQL, PostgreSQL, SQLite, MS SQL and Oracle.';
+	
+	//menu details
+		$apps[$x]['menu'][0]['title']['en'] = 'Adminer';
+		$apps[$x]['menu'][0]['guid'] = '1F59D07B-B4F7-4F9E-BDE9-312CF491D66E';
+		$apps[$x]['menu'][0]['parent_guid'] = '594D99C5-6128-9C88-CA35-4B33392CEC0F';
+		$apps[$x]['menu'][0]['category'] = 'external';
+		$apps[$x]['menu'][0]['path'] = '/mod/adminer/index.php';
+		$apps[$x]['menu'][0]['groups'][] = 'superadmin';
+
+?>
