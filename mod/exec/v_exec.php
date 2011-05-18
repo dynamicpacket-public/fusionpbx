f634ec4e5f70388b3b676f0155cfef80878b119a
markjcrane <markjcrane@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2011-05-18 12:12:59 +0000

    Add the granular permissions to the advanced -> command page.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk/fusionpbx@1103 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- mod/exec/v_exec.php
+++ mod/exec/v_exec.php
@@ -26,8 +26,7 @@
 include "root.php";
 require_once "includes/config.php";
 require_once "includes/checkauth.php";
-
-if (ifgroup("superadmin")) {
+if (permission_exists('exec_command_line') || permission_exists('exec_php_command') || permission_exists('exec_switch')) {
 	//access granted
 }
 else {
@@ -116,104 +115,101 @@ if (count($_POST)>0) {
 	//echo "	<input type='button' class='btn' name='' alt='back' onclick=\"window.location='index.php'\" value='Back'>\n";
 	echo "</td>\n";
 	echo "</tr>\n";
-
-	echo "<tr>\n";
-	echo "<td class='vncell' valign='top' align='left' nowrap>\n";
-	echo "    Shell command:\n";
-	echo "</td>\n";
-	echo "<td class='vtable' align='left'>\n";
-	echo "    <textarea name='shellcmd' id='shellcmd' rows='7' class='txt' wrap='off'>$shellcmd</textarea\n";
-	echo "<br />\n";
-	//echo "\n";
-	echo "</td>\n";
-	echo "</tr>\n";
-
-	echo "<tr>\n";
-	echo "<td class='vncell' valign='top' align='left' nowrap>\n";
-	echo "    PHP command:\n";
-	echo "</td>\n";
-	echo "<td class='vtable' align='left'>\n";
-	echo "    <textarea name='phpcmd' id='phpcmd' rows='10' class='txt' wrap='off'>$phpcmd</textarea\n";
-	echo "<br />\n";
-	echo "Use the following link as a reference for PHP: <a href='http://php.net/manual/en/index.php' target='_blank'>PHP Manual</a>\n";
-	echo "</td>\n";
-	echo "</tr>\n";
-
-
-	echo "<tr>\n";
-	echo "<td class='vncell' valign='top' align='left' nowrap>\n";
-	if ($v_name == "freeswitch") {
-		echo "    Switch Command:\n";
+	if (permission_exists('exec_command_line')) {
+		echo "<tr>\n";
+		echo "<td class='vncell' valign='top' align='left' nowrap>\n";
+		echo "    Shell command:\n";
+		echo "</td>\n";
+		echo "<td class='vtable' align='left'>\n";
+		echo "    <textarea name='shellcmd' id='shellcmd' rows='7' class='txt' wrap='off'>$shellcmd</textarea\n";
+		echo "<br />\n";
+		echo "</td>\n";
+		echo "</tr>\n";
 	}
-	else {
-		echo "    ".ucfirst($v_name)." Command:\n";
+	if (permission_exists('exec_php_command')) {
+		echo "<tr>\n";
+		echo "<td class='vncell' valign='top' align='left' nowrap>\n";
+		echo "    PHP command:\n";
+		echo "</td>\n";
+		echo "<td class='vtable' align='left'>\n";
+		echo "    <textarea name='phpcmd' id='phpcmd' rows='10' class='txt' wrap='off'>$phpcmd</textarea\n";
+		echo "<br />\n";
+		echo "Use the following link as a reference for PHP: <a href='http://php.net/manual/en/index.php' target='_blank'>PHP Manual</a>\n";
+		echo "</td>\n";
+		echo "</tr>\n";
 	}
-	echo "</td>\n";
-	echo "<td class='vtable' align='left'>\n";
-	echo "    <textarea name='switchcmd' id='switchcmd' rows='7' class='txt' wrap='off'>$switchcmd</textarea\n";
-	echo "<br />\n";
-	echo "For a list of the valid commands use: help\n";
-	echo "</td>\n";
-	echo "</tr>\n";
-
+	if (permission_exists('exec_switch')) {
+		echo "<tr>\n";
+		echo "<td class='vncell' valign='top' align='left' nowrap>\n";
+		if ($v_name == "freeswitch") {
+			echo "    Switch Command:\n";
+		}
+		else {
+			echo "    ".ucfirst($v_name)." Command:\n";
+		}
+		echo "</td>\n";
+		echo "<td class='vtable' align='left'>\n";
+		echo "    <textarea name='switchcmd' id='switchcmd' rows='7' class='txt' wrap='off'>$switchcmd</textarea\n";
+		echo "<br />\n";
+		echo "For a list of the valid commands use: help\n";
+		echo "</td>\n";
+		echo "</tr>\n";
+	}
 	echo "	<tr>\n";
 	echo "		<td colspan='2' align='right'>\n";
 	echo "			<input type='submit' name='submit' class='btn' value='Execute'>\n";
 	echo "		</td>\n";
 	echo "	</tr>";
-
 	echo "</table>";
 
 //POST to PHP variables
-if (count($_POST)>0) {
+	if (count($_POST)>0) {
 
-	echo "	<tr>\n";
-	echo "		<td colspan='2' align='left'>\n";
+		echo "	<tr>\n";
+		echo "		<td colspan='2' align='left'>\n";
 
-	//shellcmd
-	if (strlen($shellcmd) > 0) {
-		echo "<b>shell command:</b>\n";
-		echo "<!--\n";
-		$shell_result = shell_exec($shellcmd);
-		echo "-->\n";
-		echo "<pre>\n";
-		echo htmlentities($shell_result);
-		echo "</pre>\n";
-	}
-
-	//phpcmd
-	if (strlen($phpcmd) > 0) {
-		echo "<b>php command:</b>\n";
-		echo "<pre>\n";
-		$php_result = eval($phpcmd);
-		echo htmlentities($php_result);
-		echo "</pre>\n";
-	}
-
-	//fs cmd
-	if (strlen($switchcmd) > 0) {
-		echo "<b>switch command:</b>\n";
-		echo "<pre>\n";
-		$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
-		if ($fp) {
-			$switch_result = event_socket_request($fp, 'api '.$switchcmd);
-			//$switch_result = eval($switchcmd);
-			echo htmlentities($switch_result);
+		//shellcmd
+		if (strlen($shellcmd) > 0 && permission_exists('exec_command_line')) {
+			echo "<b>shell command:</b>\n";
+			echo "<!--\n";
+			$shell_result = shell_exec($shellcmd);
+			echo "-->\n";
+			echo "<pre>\n";
+			echo htmlentities($shell_result);
+			echo "</pre>\n";
 		}
-		echo "</pre>\n";
-	}
 
-	echo "		</td>\n";
-	echo "	</tr>";
-}
+		//phpcmd
+		if (strlen($phpcmd) > 0 && permission_exists('exec_php_command')) {
+			echo "<b>php command:</b>\n";
+			echo "<pre>\n";
+			$php_result = eval($phpcmd);
+			echo htmlentities($php_result);
+			echo "</pre>\n";
+		}
+
+		//fs cmd
+		if (strlen($switchcmd) > 0 && permission_exists('exec_switch')) {
+			echo "<b>switch command:</b>\n";
+			echo "<pre>\n";
+			$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
+			if ($fp) {
+				$switch_result = event_socket_request($fp, 'api '.$switchcmd);
+				//$switch_result = eval($switchcmd);
+				echo htmlentities($switch_result);
+			}
+			echo "</pre>\n";
+		}
+		echo "		</td>\n";
+		echo "	</tr>";
+	}
 
 	echo "	</td>";
 	echo "	</tr>";
 	echo "</table>";
 	echo "</div>";
-
 	echo "</form>";
 
-
-require_once "includes/footer.php";
+//show the footer
+	require_once "includes/footer.php";
 ?>
