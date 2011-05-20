e3d4885a8bcb2acc06089543b6381af8e4047f27
markjcrane <markjcrane@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2011-05-20 03:59:09 +0000

    Update the Advanced -> Command page. Move the command results to the top of the page decrease the size of the shell and switch command boxes and improve the code quality.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk/fusionpbx@1122 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- mod/exec/v_exec.php
+++ mod/exec/v_exec.php
@@ -34,107 +34,146 @@ else {
 	exit;
 }
 
-if (count($_POST)>0) {
-	$shellcmd = trim($_POST["shellcmd"]);
-	$phpcmd = trim($_POST["phpcmd"]);
-	$switchcmd = trim($_POST["switchcmd"]);
-}
+//get the html values and set them as variables
+	if (count($_POST)>0) {
+		$shell_cmd = trim($_POST["shell_cmd"]);
+		$php_cmd = trim($_POST["php_cmd"]);
+		$switch_cmd = trim($_POST["switch_cmd"]);
+	}
 
-
+//show the header
 	require_once "includes/header.php";
 
+//edit area
+	echo "    <script language=\"javascript\" type=\"text/javascript\" src=\"".PROJECT_PATH."/includes/edit_area/edit_area_full.js\"></script>\n";
+	echo "    <!-- -->\n";
 
-	//--- Begin: Edit Area -----------------------------------------------------
-		echo "    <script language=\"javascript\" type=\"text/javascript\" src=\"".PROJECT_PATH."/includes/edit_area/edit_area_full.js\"></script>\n";
-		echo "    <!-- -->\n";
+	echo "	<script language=\"Javascript\" type=\"text/javascript\">\n";
+	echo "		// initialisation //load,\n";
+	echo "		editAreaLoader.init({\n";
+	echo "			id: \"shell_cmd\"	// id of the textarea to transform //, |, help\n";
+	echo "			,start_highlight: false\n";
+	echo "			,display: \"later\"\n";
+	echo "			,font_size: \"8\"\n";
+	echo "			,allow_toggle: true\n";
+	echo "			,language: \"en\"\n";
+	echo "			,syntax: \"html\"\n";
+	echo "			,toolbar: \"search, go_to_line,|, fullscreen, |, undo, redo, |, select_font, |, syntax_selection, |, change_smooth_selection, highlight, reset_highlight, |, help\" //new_document,\n";
+	echo "			,plugins: \"charmap\"\n";
+	echo "			,charmap_default: \"arrows\"\n";
+	echo "\n";
+	echo "    });\n";
+	echo "\n";
+	echo "\n";
+	echo "		editAreaLoader.init({\n";
+	echo "			id: \"php_cmd\"	// id of the textarea to transform //, |, help\n";
+	echo "			,start_highlight: false\n";
+	echo "			,display: \"later\"\n";
+	echo "			,font_size: \"8\"\n";
+	echo "			,allow_toggle: true\n";
+	echo "			,language: \"en\"\n";
+	echo "			,syntax: \"php\"\n";
+	echo "			,toolbar: \"search, go_to_line,|, fullscreen, |, undo, redo, |, select_font, |, syntax_selection, |, change_smooth_selection, highlight, reset_highlight, |, help\" //new_document,\n";
+	echo "			,plugins: \"charmap\"\n";
+	echo "			,charmap_default: \"arrows\"\n";
+	echo "\n";
+	echo "    });\n";
+	echo "\n";
+	echo "		editAreaLoader.init({\n";
+	echo "			id: \"switch_cmd\"	// id of the textarea to transform //, |, help\n";
+	echo "			,start_highlight: false\n";
+	echo "			,display: \"later\"\n";
+	echo "			,font_size: \"8\"\n";
+	echo "			,allow_toggle: true\n";
+	echo "			,language: \"en\"\n";
+	echo "			,syntax: \"php\"\n";
+	echo "			,toolbar: \"search, go_to_line,|, fullscreen, |, undo, redo, |, select_font, |, syntax_selection, |, change_smooth_selection, highlight, reset_highlight, |, help\" //new_document,\n";
+	echo "			,plugins: \"charmap\"\n";
+	echo "			,charmap_default: \"arrows\"\n";
+	echo "\n";
+	echo "    });\n";
+	echo "    </script>";
 
-		echo "	<script language=\"Javascript\" type=\"text/javascript\">\n";
-		echo "		// initialisation //load,\n";
-		echo "		editAreaLoader.init({\n";
-		echo "			id: \"shellcmd\"	// id of the textarea to transform //, |, help\n";
-		echo "			,start_highlight: false\n";
-		echo "			,display: \"later\"\n";
-		echo "			,font_size: \"8\"\n";
-		echo "			,allow_toggle: true\n";
-		echo "			,language: \"en\"\n";
-		echo "			,syntax: \"html\"\n";
-		echo "			,toolbar: \"search, go_to_line,|, fullscreen, |, undo, redo, |, select_font, |, syntax_selection, |, change_smooth_selection, highlight, reset_highlight, |, help\" //new_document,\n";
-		echo "			,plugins: \"charmap\"\n";
-		echo "			,charmap_default: \"arrows\"\n";
-		echo "\n";
-		echo "    });\n";
-		echo "\n";
-		echo "\n";
-		echo "		editAreaLoader.init({\n";
-		echo "			id: \"phpcmd\"	// id of the textarea to transform //, |, help\n";
-		echo "			,start_highlight: false\n";
-		echo "			,display: \"later\"\n";
-		echo "			,font_size: \"8\"\n";
-		echo "			,allow_toggle: true\n";
-		echo "			,language: \"en\"\n";
-		echo "			,syntax: \"php\"\n";
-		echo "			,toolbar: \"search, go_to_line,|, fullscreen, |, undo, redo, |, select_font, |, syntax_selection, |, change_smooth_selection, highlight, reset_highlight, |, help\" //new_document,\n";
-		echo "			,plugins: \"charmap\"\n";
-		echo "			,charmap_default: \"arrows\"\n";
-		echo "\n";
-		echo "    });\n";
-		echo "\n";
-		echo "		editAreaLoader.init({\n";
-		echo "			id: \"switchcmd\"	// id of the textarea to transform //, |, help\n";
-		echo "			,start_highlight: false\n";
-		echo "			,display: \"later\"\n";
-		echo "			,font_size: \"8\"\n";
-		echo "			,allow_toggle: true\n";
-		echo "			,language: \"en\"\n";
-		echo "			,syntax: \"php\"\n";
-		echo "			,toolbar: \"search, go_to_line,|, fullscreen, |, undo, redo, |, select_font, |, syntax_selection, |, change_smooth_selection, highlight, reset_highlight, |, help\" //new_document,\n";
-		echo "			,plugins: \"charmap\"\n";
-		echo "			,charmap_default: \"arrows\"\n";
-		echo "\n";
-		echo "    });\n";
-		echo "    </script>";
-	//--- End: Edit Area -------------------------------------------------------
-
+//show the header
 	echo "<div align='center'>";
 	echo "<table width='100%' border='0' cellpadding='0' cellspacing='2'>\n";
+	echo "<tr>\n";
+	echo "<td colspan='2' align='left' nowrap><b>Execute Command</b></td>\n";
+	echo "</tr>\n";
+	echo "<tr>\n";
+	echo "	<td colspan='2'>\n";
+	echo "		Provides a conventient way to execute system, PHP, and switch commands.\n";
+	echo "	<br />\n";
+	echo "	<br />\n";
+	echo "	</td>\n";
+	echo "</tr>\n";
 
-	echo "<tr class='border'>\n";
-	echo "	<td align=\"left\">\n";
-	echo "      <br>";
+//show the result
+	if (count($_POST)>0) {
+		echo "	<tr>\n";
+		echo "		<td colspan='2' align='left'>\n";
 
+		//shell_cmd
+		if (strlen($shell_cmd) > 0 && permission_exists('exec_command_line')) {
+			echo "<b>$shell_cmd</b>\n";
+			echo "<!--\n";
+			$shell_result = shell_exec($shell_cmd);
+			echo "-->\n";
+			echo "<pre>";
+			echo htmlentities($shell_result);
+			echo "</pre>\n";
+		}
 
+		//php_cmd
+		if (strlen($php_cmd) > 0 && permission_exists('exec_php_command')) {
+			//echo "<b></b>\n";
+			echo "<pre>";
+			$php_result = eval($php_cmd);
+			echo htmlentities($php_result);
+			echo "</pre>\n";
+		}
 
+		//fs cmd
+		if (strlen($switch_cmd) > 0 && permission_exists('exec_switch')) {
+			echo "<b>$switch_cmd</b>\n";
+			echo "<pre>";
+			$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
+			if ($fp) {
+				$switch_result = event_socket_request($fp, 'api '.$switch_cmd);
+				//$switch_result = eval($switch_cmd);
+				echo htmlentities($switch_result);
+			}
+			echo "</pre>\n";
+		}
+		echo "			<br />\n";
+		echo "		</td>\n";
+		echo "	</tr>";
+	}
+
+//html form
 	echo "<form method='post' name='frm' action=''>\n";
 
-	echo "<div align='center'>\n";
-	echo "<table width='100%'  border='0' cellpadding='6' cellspacing='0'>\n";
-
-	echo "<tr>\n";
-	echo "<td align='left' width='30%' nowrap><b>Execute Command</b></td>\n";
-	echo "<td width='70%' align='right'>\n";
-	//echo "	<input type='button' class='btn' name='' alt='back' onclick=\"window.location='index.php'\" value='Back'>\n";
-	echo "</td>\n";
-	echo "</tr>\n";
 	if (permission_exists('exec_command_line')) {
 		echo "<tr>\n";
 		echo "<td class='vncell' valign='top' align='left' nowrap>\n";
-		echo "    Shell command:\n";
+		echo "	Shell command:\n";
 		echo "</td>\n";
 		echo "<td class='vtable' align='left'>\n";
-		echo "    <textarea name='shellcmd' id='shellcmd' rows='7' class='txt' wrap='off'>$shellcmd</textarea\n";
-		echo "<br />\n";
+		echo "	<textarea name='shell_cmd' id='shell_cmd' rows='2' class='txt' wrap='off'>$shell_cmd</textarea\n";
+		echo "	<br />\n";
+		echo "	System commands.\n";
 		echo "</td>\n";
 		echo "</tr>\n";
 	}
 	if (permission_exists('exec_php_command')) {
 		echo "<tr>\n";
 		echo "<td class='vncell' valign='top' align='left' nowrap>\n";
-		echo "    PHP command:\n";
+		echo "	PHP command:\n";
 		echo "</td>\n";
 		echo "<td class='vtable' align='left'>\n";
-		echo "    <textarea name='phpcmd' id='phpcmd' rows='10' class='txt' wrap='off'>$phpcmd</textarea\n";
-		echo "<br />\n";
-		echo "Use the following link as a reference for PHP: <a href='http://php.net/manual/en/index.php' target='_blank'>PHP Manual</a>\n";
+		echo "	<textarea name='php_cmd' id='php_cmd' rows='7' class='txt' wrap='off'>$php_cmd</textarea\n";
+		echo "	<br />\n";
+		echo "	Use the following link as a reference for PHP: <a href='http://php.net/manual/en/index.php' target='_blank'>PHP Manual</a>\n";
 		echo "</td>\n";
 		echo "</tr>\n";
 	}
@@ -142,16 +181,16 @@ if (count($_POST)>0) {
 		echo "<tr>\n";
 		echo "<td class='vncell' valign='top' align='left' nowrap>\n";
 		if ($v_name == "freeswitch") {
-			echo "    Switch Command:\n";
+			echo "	Switch Command:\n";
 		}
 		else {
-			echo "    ".ucfirst($v_name)." Command:\n";
+			echo "	".ucfirst($v_name)." Command:\n";
 		}
 		echo "</td>\n";
 		echo "<td class='vtable' align='left'>\n";
-		echo "    <textarea name='switchcmd' id='switchcmd' rows='7' class='txt' wrap='off'>$switchcmd</textarea\n";
-		echo "<br />\n";
-		echo "For a list of the valid commands use: help\n";
+		echo "	<textarea name='switch_cmd' id='switch_cmd' rows='2' class='txt' wrap='off'>$switch_cmd</textarea\n";
+		echo "	<br />\n";
+		echo "	For a list of the valid commands use: help\n";
 		echo "</td>\n";
 		echo "</tr>\n";
 	}
@@ -160,55 +199,10 @@ if (count($_POST)>0) {
 	echo "			<input type='submit' name='submit' class='btn' value='Execute'>\n";
 	echo "		</td>\n";
 	echo "	</tr>";
-	echo "</table>";
+	echo "</form>";
 
-//POST to PHP variables
-	if (count($_POST)>0) {
-
-		echo "	<tr>\n";
-		echo "		<td colspan='2' align='left'>\n";
-
-		//shellcmd
-		if (strlen($shellcmd) > 0 && permission_exists('exec_command_line')) {
-			echo "<b>shell command:</b>\n";
-			echo "<!--\n";
-			$shell_result = shell_exec($shellcmd);
-			echo "-->\n";
-			echo "<pre>\n";
-			echo htmlentities($shell_result);
-			echo "</pre>\n";
-		}
-
-		//phpcmd
-		if (strlen($phpcmd) > 0 && permission_exists('exec_php_command')) {
-			echo "<b>php command:</b>\n";
-			echo "<pre>\n";
-			$php_result = eval($phpcmd);
-			echo htmlentities($php_result);
-			echo "</pre>\n";
-		}
-
-		//fs cmd
-		if (strlen($switchcmd) > 0 && permission_exists('exec_switch')) {
-			echo "<b>switch command:</b>\n";
-			echo "<pre>\n";
-			$fp = event_socket_create($_SESSION['event_socket_ip_address'], $_SESSION['event_socket_port'], $_SESSION['event_socket_password']);
-			if ($fp) {
-				$switch_result = event_socket_request($fp, 'api '.$switchcmd);
-				//$switch_result = eval($switchcmd);
-				echo htmlentities($switch_result);
-			}
-			echo "</pre>\n";
-		}
-		echo "		</td>\n";
-		echo "	</tr>";
-	}
-
-	echo "	</td>";
-	echo "	</tr>";
 	echo "</table>";
 	echo "</div>";
-	echo "</form>";
 
 //show the footer
 	require_once "includes/footer.php";
