45a68a650b15086e395e80de5f6baa27376b8f6f
markjcrane <markjcrane@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2011-05-22 00:39:31 +0000

    Add granular permissions to status -> system status.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk/fusionpbx@1145 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- mod/system/system.php
+++ mod/system/system.php
@@ -26,7 +26,11 @@
 include "root.php";
 require_once "includes/config.php";
 require_once "includes/checkauth.php";
-if (ifgroup("superadmin")) {
+if (permission_exists('system_view_info') 
+	|| permission_exists('system_view_cpu') 
+	|| permission_exists('system_view_hdd') 
+	|| permission_exists('system_view_ram') 
+	|| permission_exists('system_view_backup')) {
 	//access granted
 }
 else {
@@ -66,47 +70,46 @@ echo "<br />";
 	echo "<tr>\n";
 	echo "	<th class='th' colspan='2' align='left'>System Information</th>\n";
 	echo "</tr>\n";
-
-	echo "<tr>\n";
-	echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-	echo "		Version: \n";
-	echo "	</td>\n";
-	echo "	<td class=\"rowstyle1\">\n";
-	echo "		".software_version()."\n";
-	echo "	</td>\n";
-	echo "</tr>\n";
-
-	echo "<!--\n";
-	$tmp_result = shell_exec('uname -a');
-	echo "-->\n";
-	if (strlen($tmp_result) > 0) {
+	if (permission_exists('system_view_info')) {
 		echo "<tr>\n";
 		echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-		echo "		Operating System: \n";
+		echo "		Version: \n";
 		echo "	</td>\n";
 		echo "	<td class=\"rowstyle1\">\n";
-		echo "		".$tmp_result." \n";
+		echo "		".software_version()."\n";
 		echo "	</td>\n";
 		echo "</tr>\n";
+
+		echo "<!--\n";
+		$tmp_result = shell_exec('uname -a');
+		echo "-->\n";
+		if (strlen($tmp_result) > 0) {
+			echo "<tr>\n";
+			echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+			echo "		Operating System: \n";
+			echo "	</td>\n";
+			echo "	<td class=\"rowstyle1\">\n";
+			echo "		".$tmp_result." \n";
+			echo "	</td>\n";
+			echo "</tr>\n";
+		}
+		unset($tmp_result);
+
+		echo "<!--\n";
+		$tmp_result = shell_exec('uptime');
+		echo "-->\n";
+		if (strlen($tmp_result) > 0) {
+			echo "<tr>\n";
+			echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+			echo "		Uptime: \n";
+			echo "	</td>\n";
+			echo "	<td class=\"rowstyle1\">\n";
+			echo "		".$tmp_result." \n";
+			echo "	</td>\n";
+			echo "</tr>\n";
+		}
+		unset($tmp_result);
 	}
-	unset($tmp_result);
-
-	echo "<!--\n";
-	$tmp_result = shell_exec('uptime');
-	echo "-->\n";
-	if (strlen($tmp_result) > 0) {
-		echo "<tr>\n";
-		echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-		echo "		Uptime: \n";
-		echo "	</td>\n";
-		echo "	<td class=\"rowstyle1\">\n";
-		echo "		".$tmp_result." \n";
-		echo "	</td>\n";
-		echo "</tr>\n";
-	}
-	unset($tmp_result);
-
-
 	echo "<tr>\n";
 	echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
 	echo "		Date: \n";
@@ -123,235 +126,207 @@ echo "<br />";
 
 
 //memory information
-	//linux
-	if (stristr(PHP_OS, 'Linux')) {
-		echo "<!--\n";
-		$shellcmd='free';
-		$shell_result = shell_exec($shellcmd);
-		echo "-->\n";
-		if (strlen($shell_result) > 0) {
-			echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
-			echo "<tr>\n";
-			echo "	<th colspan='2' align='left' valign='top'>Memory Information</th>\n";
-			echo "</tr>\n";
-			echo "<tr>\n";
-			echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-			echo "	Memory Status:\n";
-			echo "	</td>\n";
-			echo "	<td class=\"rowstyle1\">\n";
-			echo "	<pre>\n";
-			echo "$shell_result<br>";
-			echo "</pre>\n";
-			unset($shell_result);
-			echo "	</td>\n";
-			echo "</tr>\n";
-			echo "</table>\n";
-			echo "<br />";
-			echo "<br />";
-			echo "<br />";
+	if (permission_exists('system_view_ram')) {
+		//linux
+		if (stristr(PHP_OS, 'Linux')) {
+			echo "<!--\n";
+			$shellcmd='free';
+			$shell_result = shell_exec($shellcmd);
+			echo "-->\n";
+			if (strlen($shell_result) > 0) {
+				echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
+				echo "<tr>\n";
+				echo "	<th colspan='2' align='left' valign='top'>Memory Information</th>\n";
+				echo "</tr>\n";
+				echo "<tr>\n";
+				echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+				echo "	Memory Status:\n";
+				echo "	</td>\n";
+				echo "	<td class=\"rowstyle1\">\n";
+				echo "	<pre>\n";
+				echo "$shell_result<br>";
+				echo "</pre>\n";
+				unset($shell_result);
+				echo "	</td>\n";
+				echo "</tr>\n";
+				echo "</table>\n";
+				echo "<br />";
+				echo "<br />";
+				echo "<br />";
+			}
+		}
+
+		//freebsd
+		if (stristr(PHP_OS, 'FreeBSD')) {
+			echo "<!--\n";
+			$shellcmd='sysctl vm.vmtotal';
+			$shell_result = shell_exec($shellcmd);
+			echo "-->\n";
+			if (strlen($shell_result) > 0) {
+				echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
+				echo "<tr>\n";
+				echo "	<th colspan='2' align='left' valign='top'>Memory Information</th>\n";
+				echo "</tr>\n";
+				echo "<tr>\n";
+				echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+				echo "	Memory Status:\n";
+				echo "	</td>\n";
+				echo "	<td class=\"rowstyle1\">\n";
+				echo "	<pre>\n";
+				echo "$shell_result<br>";
+				echo "</pre>\n";
+				unset($shell_result);
+				echo "	</td>\n";
+				echo "</tr>\n";
+				echo "</table>\n";
+				echo "<br />";
+				echo "<br />";
+				echo "<br />";
+			}
 		}
 	}
 
-	//freebsd
-	if (stristr(PHP_OS, 'FreeBSD')) {
-		echo "<!--\n";
-		$shellcmd='sysctl vm.vmtotal';
-		$shell_result = shell_exec($shellcmd);
-		echo "-->\n";
-		if (strlen($shell_result) > 0) {
-			echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
-			echo "<tr>\n";
-			echo "	<th colspan='2' align='left' valign='top'>Memory Information</th>\n";
-			echo "</tr>\n";
-			echo "<tr>\n";
-			echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-			echo "	Memory Status:\n";
-			echo "	</td>\n";
-			echo "	<td class=\"rowstyle1\">\n";
-			echo "	<pre>\n";
-			echo "$shell_result<br>";
-			echo "</pre>\n";
-			unset($shell_result);
-			echo "	</td>\n";
-			echo "</tr>\n";
-			echo "</table>\n";
-			echo "<br />";
-			echo "<br />";
-			echo "<br />";
+//cpu information
+	if (permission_exists('system_view_cpu')) {
+		//linux
+		if (stristr(PHP_OS, 'Linux')) {
+			echo "<!--\n";
+			$shellcmd="ps -e -o pcpu,cpu,nice,state,cputime,args --sort pcpu | sed '/^ 0.0 /d'";
+			$shell_result = shell_exec($shellcmd);
+			echo "-->\n";
+			if (strlen($shell_result) > 0) {
+				echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
+				echo "<tr>\n";
+				echo "	<th class='th' colspan='2' align='left' valign='top'>CPU Information</th>\n";
+				echo "</tr>\n";
+				echo "<tr>\n";
+				echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+				echo "	CPU Status:\n";
+				echo "	</td>\n";
+				echo "	<td class=\"rowstyle1\">\n";
+				echo "	<pre>\n";
+
+				//$last_line = shell_exec($shellcmd, $shell_result);
+				//foreach ($shell_result as $value) {
+				//	echo substr($value, 0, 100);
+				//	echo "<br />";
+				//}
+
+				echo "$shell_result<br>";
+
+				echo "</pre>\n";
+				unset($shell_result);
+				echo "	</td>\n";
+				echo "</tr>\n";
+				echo "</table>\n";
+				echo "<br />";
+				echo "<br />";
+				echo "<br />";
+			}
 		}
-	}
 
-//memory information
-	//linux
-	if (stristr(PHP_OS, 'Linux')) {
-		echo "<!--\n";
-		$shellcmd="ps -e -o pcpu,cpu,nice,state,cputime,args --sort pcpu | sed '/^ 0.0 /d'";
-		$shell_result = shell_exec($shellcmd);
-		echo "-->\n";
-		if (strlen($shell_result) > 0) {
-			echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
-			echo "<tr>\n";
-			echo "	<th class='th' colspan='2' align='left' valign='top'>CPU Information</th>\n";
-			echo "</tr>\n";
-			echo "<tr>\n";
-			echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-			echo "	CPU Status:\n";
-			echo "	</td>\n";
-			echo "	<td class=\"rowstyle1\">\n";
-			echo "	<pre>\n";
-
-			//$last_line = shell_exec($shellcmd, $shell_result);
-			//foreach ($shell_result as $value) {
-			//	echo substr($value, 0, 100);
-			//	echo "<br />";
-			//}
-
-			echo "$shell_result<br>";
-
-			echo "</pre>\n";
-			unset($shell_result);
-			echo "	</td>\n";
-			echo "</tr>\n";
-			echo "</table>\n";
-			echo "<br />";
-			echo "<br />";
-			echo "<br />";
-		}
-	}
-
-	//freebsd
-	if (stristr(PHP_OS, 'FreeBSD')) {
-		echo "<!--\n";
-		$shellcmd='top';
-		$shell_result = shell_exec($shellcmd);
-		echo "-->\n";
-		if (strlen($shell_result) > 0) {
-			echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
-			echo "<tr>\n";
-			echo "	<th class='th' colspan='2' align='left' valign='top'>CPU Information</th>\n";
-			echo "</tr>\n";
-			echo "<tr>\n";
-			echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-			echo "	CPU Status:\n";
-			echo "	</td>\n";
-			echo "	<td class=\"rowstyle1\">\n";
-			echo "	<pre>\n";
-			echo "$shell_result<br>";
-			echo "</pre>\n";
-			unset($shell_result);
-			echo "	</td>\n";
-			echo "</tr>\n";
-			echo "</table>\n";
-			echo "<br />";
-			echo "<br />";
-			echo "<br />";
+		//freebsd
+		if (stristr(PHP_OS, 'FreeBSD')) {
+			echo "<!--\n";
+			$shellcmd='top';
+			$shell_result = shell_exec($shellcmd);
+			echo "-->\n";
+			if (strlen($shell_result) > 0) {
+				echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
+				echo "<tr>\n";
+				echo "	<th class='th' colspan='2' align='left' valign='top'>CPU Information</th>\n";
+				echo "</tr>\n";
+				echo "<tr>\n";
+				echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+				echo "	CPU Status:\n";
+				echo "	</td>\n";
+				echo "	<td class=\"rowstyle1\">\n";
+				echo "	<pre>\n";
+				echo "$shell_result<br>";
+				echo "</pre>\n";
+				unset($shell_result);
+				echo "	</td>\n";
+				echo "</tr>\n";
+				echo "</table>\n";
+				echo "<br />";
+				echo "<br />";
+				echo "<br />";
+			}
 		}
 	}
 
 //drive space
-	if (stristr(PHP_OS, 'Linux') || stristr(PHP_OS, 'FreeBSD')) {
-		echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
-		echo "<tr>\n";
-		echo "	<th class='th' colspan='2' align='left'>Drive Information</th>\n";
-		echo "</tr>\n";
-		echo "<tr>\n";
-		echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-		echo "		Drive Space: \n";
-		echo "	</td>\n";
-		echo "	<td class=\"rowstyle1\">\n";
-		echo "<pre>\n";
-		$shellcmd = 'df -h';
-		$shell_result = shell_exec($shellcmd);
-		echo "$shell_result<br>";
-		echo "</pre>\n";
-		echo "	</td>\n";
-		echo "</tr>\n";
-		echo "</table>\n";
-	} else if (stristr(PHP_OS, 'WIN')) {
-		//disk_free_space returns the number of bytes available on the drive;
-		//1 kilobyte = 1024 byte
-		//1 megabyte = 1024 kilobyte		
-		$driveletter = substr($_SERVER["DOCUMENT_ROOT"], 0, 2);
-		$disksize = round(disk_total_space($driveletter)/1024/1024, 2);
-		$disksizefree = round(disk_free_space($driveletter)/1024/1024, 2);
-		$diskpercentavailable = round(($disksizefree/$disksize) * 100, 2);
+	if (permission_exists('system_view_hdd')) {
+		if (stristr(PHP_OS, 'Linux') || stristr(PHP_OS, 'FreeBSD')) {
+			echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
+			echo "<tr>\n";
+			echo "	<th class='th' colspan='2' align='left'>Drive Information</th>\n";
+			echo "</tr>\n";
+			echo "<tr>\n";
+			echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+			echo "		Drive Space: \n";
+			echo "	</td>\n";
+			echo "	<td class=\"rowstyle1\">\n";
+			echo "<pre>\n";
+			$shellcmd = 'df -h';
+			$shell_result = shell_exec($shellcmd);
+			echo "$shell_result<br>";
+			echo "</pre>\n";
+			echo "	</td>\n";
+			echo "</tr>\n";
+			echo "</table>\n";
+		} else if (stristr(PHP_OS, 'WIN')) {
+			//disk_free_space returns the number of bytes available on the drive;
+			//1 kilobyte = 1024 byte
+			//1 megabyte = 1024 kilobyte		
+			$driveletter = substr($_SERVER["DOCUMENT_ROOT"], 0, 2);
+			$disksize = round(disk_total_space($driveletter)/1024/1024, 2);
+			$disksizefree = round(disk_free_space($driveletter)/1024/1024, 2);
+			$diskpercentavailable = round(($disksizefree/$disksize) * 100, 2);
 
-		echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
-		echo "<tr>\n";
-		echo "	<th class='th' colspan='2' align='left'>Drive Space</th>\n";
-		echo "</tr>\n";
-		echo "<tr>\n";
-		echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-		echo "		Capacity: \n";
-		echo "	</td>\n";
-		echo "	<td class=\"rowstyle1\">\n";
-		echo "		$disksize mb\n";
-		echo "	</td>\n";
-		echo "</tr>\n";
+			echo "<table width=\"100%\" border=\"0\" cellpadding=\"7\" cellspacing=\"0\">\n";
+			echo "<tr>\n";
+			echo "	<th class='th' colspan='2' align='left'>Drive Space</th>\n";
+			echo "</tr>\n";
+			echo "<tr>\n";
+			echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+			echo "		Capacity: \n";
+			echo "	</td>\n";
+			echo "	<td class=\"rowstyle1\">\n";
+			echo "		$disksize mb\n";
+			echo "	</td>\n";
+			echo "</tr>\n";
 
-		echo "<tr>\n";
-		echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-		echo "		Free Space: \n";
-		echo "	</td>\n";
-		echo "	<td class=\"rowstyle1\">\n";
-		echo "		$disksizefree mb\n";
-		echo "	</td>\n";
-		echo "</tr>\n";
+			echo "<tr>\n";
+			echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+			echo "		Free Space: \n";
+			echo "	</td>\n";
+			echo "	<td class=\"rowstyle1\">\n";
+			echo "		$disksizefree mb\n";
+			echo "	</td>\n";
+			echo "</tr>\n";
 
-		echo "<tr>\n";
-		echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
-		echo "		Percent Free: \n";
-		echo "	</td>\n";
-		echo "	<td class=\"rowstyle1\">\n";
-		echo "		$diskpercentavailable% \n";
-		echo "	</td>\n";
-		echo "</tr>\n";
-		echo "</table>\n";
-		echo "\n";
+			echo "<tr>\n";
+			echo "	<td width='20%' class=\"vncell\" style='text-align: left;'>\n";
+			echo "		Percent Free: \n";
+			echo "	</td>\n";
+			echo "	<td class=\"rowstyle1\">\n";
+			echo "		$diskpercentavailable% \n";
+			echo "	</td>\n";
+			echo "</tr>\n";
+			echo "</table>\n";
+			echo "\n";
+		}
+		echo "<br />";
+		echo "<br />";
+		echo "<br />";
 	}
-	echo "<br />";
-	echo "<br />";
-	echo "<br />";
-
-/*
-if (ifgroup("user")) {
-	//find the conference extensions from the dialplan include details
-
-		//define the conference array
-			$conference_array = array ();
-
-		$sql = "";
-		$sql .= "select * from v_dialplan_includes_details ";
-		$sql .= "where v_id = '$v_id' ";
-		$prepstatement = $db->prepare(check_sql($sql));
-		$prepstatement->execute();
-		$x = 0;
-		$result = $prepstatement->fetchAll();
-		foreach ($result as &$row) {
-			$dialplan_include_id = $row["dialplan_include_id"];
-			//$tag = $row["tag"];
-			//$fieldorder = $row["fieldorder"];
-			$fieldtype = $row["fieldtype"];
-			//$fielddata = $row["fielddata"];
-			if ($fieldtype == "conference") {
-				//echo "dialplan_include_id: $dialplan_include_id<br />";
-				//echo "fielddata: $fielddata<br />";
-				$conference_array[$x]['dialplan_include_id'] = $dialplan_include_id;
-				$x++;
-			}
-		}
-		unset ($prepstatement);
-		//print_r($conference_array);
-		foreach ($conference_array as &$row) {
-			echo "--".$row['dialplan_include_id']."--<br />\n";
-		}
-}
-*/
 
 //backup
-	if ($db_type == 'sqlite') {
+	if (permission_exists('zzz') && $db_type == 'sqlite') {
 		require_once "core/backup/backupandrestore.php";
 	}
 
-require_once "includes/footer.php";
+//include the footer
+	require_once "includes/footer.php";
 ?>
