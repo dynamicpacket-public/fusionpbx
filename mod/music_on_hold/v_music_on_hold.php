b289173a7c2b61fd021e3117165fdc544c829fc9
markjcrane <markjcrane@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2011-06-18 20:44:02 +0000

    Issue 113: music on hold is always inaccessible for custom groups http://code.google.com/p/fusionpbx/issues/detail?id=113
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk/fusionpbx@1250 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- mod/music_on_hold/v_music_on_hold.php
+++ mod/music_on_hold/v_music_on_hold.php
@@ -35,7 +35,6 @@ else {
 }
 
 require_once "includes/paging.php";
-require_once "includes/v_dialplan_entry_exists.php";
 
 $dir_music_on_hold_8000 = $v_sounds_dir.'/music/8000';
 ini_set(max_execution_time,7200);
