9f124fb8323b4f465785a1fdee1838953a95f899
krice@tollfreegateway.com <krice@tollfreegateway.com@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2011-07-09 21:23:25 +0000

    fix double Billing Address
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk/fusionpbx@1355 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- mod/signup/v_fields.php
+++ mod/signup/v_fields.php
@@ -19,7 +19,7 @@
 	$forms[2]['fields'][5] = array('userbillingcountry', "Country:", "text", TRUE, "Please provide a country.<br>\n");
 	$forms[2]['fields'][6] = array('userbillingpostalcode', "ZIP/Postal Code:", "text", TRUE, "Please provide a postal code.<br>\n");
 
-	$forms[3]['header'] = "Billing Address";
+	$forms[3]['header'] = "Shipping Address";
 	$forms[3]['fields'][1] = array('usershippingaddress1', "Address 1:", "text", TRUE, "Please provide a street address.<br>\n");
 	$forms[3]['fields'][2] = array('usershippingaddress2', "Address 2:", "text", FALSE, "");
 	$forms[3]['fields'][3] = array('usershippingcity', "City:", "text", TRUE, "Please provide a city.<br>\n");
