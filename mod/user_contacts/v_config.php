15788e0fce0e78d1941f937024110cab822d2045
markjcrane <markjcrane@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2011-08-21 07:24:43 +0000

    Update the menu for the newly renamed User Contacts. This tool was renamed to make room for the new contacts tool.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk/fusionpbx@1490 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- mod/user_contacts/v_config.php
+++ mod/user_contacts/v_config.php
@@ -1,20 +1,20 @@
 <?php
 	//application details
-		$apps[$x]['name'] = "Contacts";
+		$apps[$x]['name'] = "User Contacts";
 		$apps[$x]['guid'] = 'C82C2D32-301F-D3BD-50AD-34D75419C778';
 		$apps[$x]['category'] = 'System';
 		$apps[$x]['subcategory'] = '';
 		$apps[$x]['version'] = '';
 		$apps[$x]['license'] = 'Mozilla Public License 1.1';
 		$apps[$x]['url'] = 'http://www.fusionpbx.com';
-		$apps[$x]['description']['en'] = 'Manage contacts.';
+		$apps[$x]['description']['en'] = 'Manage User Contacts.';
 
 	//menu details
-		$apps[$x]['menu'][0]['title']['en'] = 'Contact Manager';
+		$apps[$x]['menu'][0]['title']['en'] = 'User Contacts';
 		$apps[$x]['menu'][0]['guid'] = '87AC2EC3-FA23-2A60-6DE6-07AC2A8AA4D9';
 		$apps[$x]['menu'][0]['parent_guid'] = 'FD29E39C-C936-F5FC-8E2B-611681B266B5';
 		$apps[$x]['menu'][0]['category'] = 'internal';
-		$apps[$x]['menu'][0]['path'] = '/mod/contacts/users.php';
+		$apps[$x]['menu'][0]['path'] = '/mod/user_contacts/users.php';
 		$apps[$x]['menu'][0]['groups'][] = 'admin';
 		$apps[$x]['menu'][0]['groups'][] = 'superadmin';
 
