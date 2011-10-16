8b53782017f7d60c8d5473f4bf32f3c76fdd27dd
markjcrane <markjcrane@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2011-10-16 03:22:54 +0000

    Added this several years ago IE=EmulateIE7 its time to remove it. When I added it I knew it would likely cause a problem in the future. This was the reason that CSS rounded corners and gradients didn't work in IE 9.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk/fusionpbx@1559 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- themes/accessible/template.php
+++ themes/accessible/template.php
@@ -15,7 +15,6 @@
 <title><!--{title}--></title>
 <!--{head}-->
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
 <style type='text/css'>
 
 img {
--- themes/default/template.php
+++ themes/default/template.php
@@ -29,7 +29,6 @@
 	}
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
 <style type='text/css'>
 
 img {
