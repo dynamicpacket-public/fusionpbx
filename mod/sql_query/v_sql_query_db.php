5b3d2892b6d5fb1d9e8e486b3d705411f46514a1
markjcrane <markjcrane@e2954ef0-caed-11de-bd98-e341d93cc3cc>
2011-10-21 05:09:45 +0000

    More alignment adjustments so that this page looks good in IE.
    git-svn-id: http://fusionpbx.googlecode.com/svn/trunk/fusionpbx@1575 e2954ef0-caed-11de-bd98-e341d93cc3cc

--- mod/sql_query/v_sql_query_db.php
+++ mod/sql_query/v_sql_query_db.php
@@ -25,11 +25,11 @@ require_once "includes/paging.php";
 
 	echo "<table width='100%' border='0'>\n";
 	echo "	<tr>\n";
-	echo "		<td width='50%' nowrap><b>Database Connections</b></td>\n";
-	echo "		<td width='50%' align='right'>&nbsp;</td>\n";
+	echo "		<td width='50%' align=\"left\" nowrap=\"nowrap\"><b>Database Connections</b></td>\n";
+	echo "		<td width='50%' align=\"right\">&nbsp;</td>\n";
 	echo "	</tr>\n";
 	echo "	<tr>\n";
-	echo "		<td colspan='2'>\n";
+	echo "		<td align=\"left\" colspan='2'>\n";
 	echo "			Select the database connection that to use.<br /><br />\n";
 	echo "		</td>\n";
 	echo "	</tr>\n";
